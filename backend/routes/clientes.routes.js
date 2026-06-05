e7887500553cbd963a905f1f63e88d25294980e7
Arnol-CR <arnolcarranza24@gmail.com>
2026-06-05 09:10:16 -0600

    feat: modulo clientes con resumen saldos e historial

--- backend/routes/clientes.routes.js
+++ backend/routes/clientes.routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
 const { buscarClientes, asignarCliente, asignarClienteMultiple, asignarPrecio, desbloquearPrecio } = require('../controllers/clientes.controller');
-router.get('/buscar', buscarClientes);
-router.post('/asignar', asignarCliente);
-router.post('/asignar-multiple', asignarClienteMultiple);
-router.post('/precio', asignarPrecio);
-router.post('/desbloquear-precio', desbloquearPrecio);
+const { getTodosClientes, getHistorialCliente } = require('../controllers/clientes.controller.nuevo');
+
+router.get('/buscar',           buscarClientes);
+router.post('/asignar',         asignarCliente);
+router.post('/asignar-multiple',asignarClienteMultiple);
+router.post('/precio',          asignarPrecio);
+router.post('/desbloquear',     desbloquearPrecio);
+router.get('/todos',            getTodosClientes);
+router.get('/historial',        getHistorialCliente);
+
 module.exports = router;
