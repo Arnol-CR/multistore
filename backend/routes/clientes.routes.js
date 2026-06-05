179cbf1b83825c8aa6a91a01d79acae66fa42d0a
Arnol-CR <arnolcarranza24@gmail.com>
2026-06-05 09:13:38 -0600

    feat: modulo clientes con resumen saldos e historial

--- backend/routes/clientes.routes.js
+++ backend/routes/clientes.routes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router  = express.Router();
-const { buscarClientes, asignarCliente, asignarClienteMultiple, asignarPrecio, desbloquearPrecio } = require('../controllers/clientes.controller');
-const { getTodosClientes, getHistorialCliente } = require('../controllers/clientes.controller.nuevo');
+const { buscarClientes, asignarCliente, asignarClienteMultiple, asignarPrecio, desbloquearPrecio, getTodosClientes, getHistorialCliente } = require('../controllers/clientes.controller');
 
-router.get('/buscar',           buscarClientes);
-router.post('/asignar',         asignarCliente);
-router.post('/asignar-multiple',asignarClienteMultiple);
-router.post('/precio',          asignarPrecio);
-router.post('/desbloquear',     desbloquearPrecio);
-router.get('/todos',            getTodosClientes);
-router.get('/historial',        getHistorialCliente);
+router.get('/buscar',            buscarClientes);
+router.post('/asignar',          asignarCliente);
+router.post('/asignar-multiple', asignarClienteMultiple);
+router.post('/precio',           asignarPrecio);
+router.post('/desbloquear-precio', desbloquearPrecio);
+router.get('/todos',             getTodosClientes);
+router.get('/historial',         getHistorialCliente);
 
 module.exports = router;
